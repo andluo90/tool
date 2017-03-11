8147b0e8f754a29e0823d57a96cd4d90ed7ba647
andluo_home <andluo90@gmail.com>
2017-03-11 16:40:38 +0800

    add send QQEmail...

--- tool.py
+++ tool.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from smtplib import SMTP_SSL,SMTPException
+from email.header import Header
+from email.mime.text import MIMEText
+
+
 def rate(a=752.35,b=24,c=5.12,d=36.47):
     """
     等额本息实际利率计算
@@ -23,4 +28,35 @@ def rate(a=752.35,b=24,c=5.12,d=36.47):
         if i in range(12,600,12):
             print("【第%d年实际年利率：%f】" %(i/12,g))
 
-rate()
+
+def send_email(subject, text,to="1455234749@qq.com"):
+    "send an email by QQ"
+
+    mailInfo = {
+        "from": "1455234749@qq.com",
+        "to": to,
+        "hostname": "smtp.qq.com",
+        "username": "1455234749@qq.com",
+        "password": "######",
+        "mailsubject": subject,
+    }
+
+    try:
+        smtp = SMTP_SSL(mailInfo["hostname"])
+        # smtp.set_debuglevel(1)
+        # smtp.ehlo(mailInfo["hostname"])
+        smtp.login(mailInfo["username"], mailInfo["password"])
+
+        msg = MIMEText(text, "plain", "UTF-8")
+        msg["Subject"] = Header(mailInfo["mailsubject"], "UTF-8")
+        msg["from"] = mailInfo["from"]
+        msg["to"] = mailInfo["to"]
+        smtp.sendmail(mailInfo["from"], mailInfo["to"], msg.as_string())
+
+        smtp.quit()
+    except SMTPException:
+        print('Send email exception')
+    else:
+        print("Send Success!!!")
+
+
