945d2796580ec97acafc5ddb509986711950ed68
andluo90 <andluo90@gmail.com>
2017-12-28 18:53:46 +0800

    Update tool.py
    简单工资计算

--- tool.py
+++ tool.py
@@ -29,7 +29,7 @@ def rate(a=752.35,b=24,c=5.12,d=36.47):
             print("【第%d年实际年利率：%f】" %(i/12,g))
 
 
-def send_email(subject, text,to="1455234749@qq.com"):
+def 发送邮件(subject, text,to="1455234749@qq.com"):
     "send an email by QQ"
 
     mailInfo = {
@@ -59,4 +59,27 @@ def send_email(subject, text,to="1455234749@qq.com"):
     else:
         print("Send Success!!!")
 
-
+def 简单工资计算(a=0000,b=196.01,c=106.50,d=0,e=22):
+    "a:税前工资,b:社保扣款,C:公积金扣款,d:请假小时数,e:出勤天数"
+    a1 = a
+    h = round(a1/e/8,2)
+    a = a - b - c-3500-h*d
+	
+    if 0 < a <= 1500:
+        a = a * 0.03 - 0
+    elif 1500 < a <= 4500:
+        a = a * 0.1 - 105
+    elif 4500 < a <= 9000:
+        a = a * 0.2 - 555
+    elif 9000 < a <=35000:
+        a = a * 0.25 - 1005
+    elif 35000 < a <= 55000:
+        a = a * 0.3 - 2755
+    elif 55000 < a <=80000:
+        a = a * 0.35 - 5505
+    elif 80000 < a:
+        a = a * 0.45 - 13505
+    print("税后工资: %0.2f" %(a1-round(a,2)-b-c-h*d))
+    print("个人所得税: %0.2f" %a)
+    if d:
+        print("请假扣钱：%0.2f" %(h*d))
